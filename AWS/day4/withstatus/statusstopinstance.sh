32920d7872f4e3e3dbd44d9e8b412e42f29a2bcc
Tarun Goel <tarun.goel1995@gmail.com>
2018-07-05 12:45:21 +0530

    modifying script to stop instances aws day4

--- AWS/day4/withstatus/statusstopinstance.sh
+++ AWS/day4/withstatus/statusstopinstance.sh
@@ -3,22 +3,22 @@
 var="$(aws ec2 describe-instances --filter Name=tag:boot-order,Values=1 | grep InstanceId | awk '{print $2}' | sed 's/"//g' | sed 's/,//g')"
 echo $var
 aws ec2 stop-instances --instance-ids $var
-status="$(aws ec2 describe-instance-status --instance-ids $var | grep Code | awk '{print $2}')"
+status="$(aws ec2 describe-instance --instance-ids $var | grep Code | awk '{print $2}')"
 echo $status
-while [ "$status" !=  "" ]
+while [ "$status" !=  "80" ]
 do
   echo $status
   sleep 20
-status="$(aws ec2 describe-instance-status --instance-ids $var | grep Code | awk '{print $2}')"
+status="$(aws ec2 describe-instance --instance-ids $var | grep Code | awk '{print $2}')"
 done
 
 
 var="$(aws ec2 describe-instances --filter Name=tag:boot-order,Values=2 | grep InstanceId | awk '{print $2}' | sed 's/"//g' | sed 's/,//g')"
 echo $var
 aws ec2 stop-instances --instance-ids $var
-status="$(aws ec2 describe-instance-status --instance-ids $var | grep Code | awk '{print $2}')"
+status="$(aws ec2 describe-instance --instance-ids $var | grep Code | awk '{print $2}')"
 echo $status
-while [ "$status" !=  "" ]
+while [ "$status" !=  "80" ]
 do
   echo $status
   sleep 20
@@ -29,7 +29,7 @@ var="$(aws ec2 describe-instances --filter Name=tag:boot-order,Values=3 | grep I
 echo $var
 aws ec2 stop-instances --instance-ids $var
 status="$(aws ec2 describe-instance-status --instance-ids $var | grep Code | awk '{print $2}')"
-while [ "$status" !=  "" ]
+while [ "$status" !=  "80" ]
 do
   echo $status
   sleep 20
